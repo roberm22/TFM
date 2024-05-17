f8648327af62bbad16fee348613bd04c30eaaa48
ROBERTO MARTIN LUENGO <r.mluengo@l092.lab.dit.upm.es>
2024-05-17 20:25:53 +0200

    v22.2 calico

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ sudo vnx -f tutorial_kubespray.xml --create
 sudo /lab/cnvr/bin/install-k8s-reqs 5.7.1
 cd ..
 cd ansible
+sleep 30
 time ansible-playbook site.yml
 cd ..
 scp -r examples k8s-master:
